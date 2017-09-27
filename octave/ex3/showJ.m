e674a790607f57cc0b5ae188d28835868864200a
bshambaugh <brent.shambaugh@gmail.com>
2017-09-27 13:27:03 -0500

    modified showJ.m to include an example that works

--- octave/ex3/showJ.m
+++ octave/ex3/showJ.m
@@ -1,9 +1,14 @@
+x = load('ex2x.dat');
+y = load('ex2y.dat');
+m = length(y);
+x = [ones(m, 1), x];
 theta = zeros(size(x(1,:)))'; % initialize fitting parameters
-alpha = %your intial learning rate%
+alpha = 0.02; %your intial learning rate%
 J = zeros(50, 1);
 
-for num_iterations = 1:50
-    J(num_iterations) = %% Calculate your cost function here %%
+for num_iterations = 1:2500
+%    J(num_iterations) = %% Calculate your cost function here %%
+     J(num_iterations)=(1.0/(2.0*m))*(x*theta-y)'*(x*theta-y);
 %    theta = %% Result of gradient descent update %%
     theta = theta - (alpha / m)*((theta'*x'-y')*(x))';
 end
@@ -11,6 +16,7 @@ end
 % now plot J
 % technically, the first J start at the zero-eth iteration
 % but Matlab/Octave doesn't have a zero index
+
 figure;
 plot(0:49, J(1:50), '-')
 xlabel('Number of iterations')
